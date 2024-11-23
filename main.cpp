58a1e123d80fc51c5b1139534f3e73d69e420bd6
KNYAAZ <119792473+KNYAAZ@users.noreply.github.com>
2024-11-23 18:58:20 +0700

    Update main.cpp
    Implemented my_pow

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 double my_pow(double base, unsigned int exponent) {
+    if (exponent == 0) return 1.0;
     double result = 1.0;
-    for (unsigned int i = 0; i < exponent; i++) {
-        result *= base;
+    while (exponent > 0) {
+        if (exponent % 2 == 1) {
+            result *= base;
+        }
+        base *= base;
+        exponent /= 2;
     }
     return result;
 }
